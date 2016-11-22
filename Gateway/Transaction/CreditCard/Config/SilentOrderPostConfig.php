b032aa31f5ab89772c70a7804ed938e093baf187
Herbert de Oliveira <beto@webjump.com.br>
2016-11-22 19:26:41 -0200

    [feature] silent order post config test

--- Gateway/Transaction/CreditCard/Config/SilentOrderPostConfig.php
+++ Gateway/Transaction/CreditCard/Config/SilentOrderPostConfig.php
@@ -8,29 +8,33 @@ class SilentOrderPostConfig implements SilentOrderPostConfigInterface
 {
 	protected $config;
 
+	protected $code;
+
 	public function __construct(
+		$code,
 		ScopeConfigInterface $config
 	){
+		$this->setCode($code);
 		$this->setConfig($config);
 	}
 
-	public function isActive($code)
+	public function isActive()
 	{
-		return $this->getConfig()->getValue("payment/{$code}/silentorderpost_active", \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+		return (boolean) $this->getConfig()->getValue("payment/{$this->getCode()}/silentorderpost_active", \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
 	}
 
-	public function getUrl($code)
+	public function getUrl()
 	{
 		if ($this->isSandBoxMode()) {
-			return $this->getConfig()->getValue("payment/{$code}/silentorderpost_url_homolog", \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+			return $this->getConfig()->getValue("payment/{$this->getCode()}/silentorderpost_url_homolog", \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
 		}
 
-		return $this->getConfig()->getValue("payment/{$code}/silentorderpost_url_production", \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+		return $this->getConfig()->getValue("payment/{$this->getCode()}/silentorderpost_url_production", \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
 	}
 
 	protected function isSandBoxMode()
 	{
-		return $this->getConfig()->getValue("payment/{$code}/silentorderpost_is_sandbox_mode", \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+		return $this->getConfig()->getValue("payment/{$this->getCode()}/silentorderpost_is_sandbox_mode", \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
 	}
 
     protected function getConfig()
@@ -41,6 +45,18 @@ class SilentOrderPostConfig implements SilentOrderPostConfigInterface
     protected function setConfig($config)
     {
         $this->config = $config;
+
+        return $this;
+    }
+
+    protected function getCode()
+    {
+        return $this->code;
+    }
+
+    protected function setCode($code)
+    {
+        $this->code = $code;
 
         return $this;
     }
