43dad89c5fca65c12306a06ab3a6baa750664cf9
Leandro Rosa <leandro@webjump.com.br>
2017-02-01 19:29:45 -0200

    #CARTRICHLO-53 #done

--- view/frontend/web/js/view/payment/method-renderer/creditcard.js
+++ view/frontend/web/js/view/payment/method-renderer/creditcard.js
@@ -9,7 +9,7 @@
 /*global define*/
 define(
     [
-        'Magento_Payment/js/view/payment/cc-form',
+        'Magento_Checkout/js/view/payment/default',
         'mage/translate',
         'Webjump_BraspagPagador/js/view/payment/method-renderer/creditcard/silentorderpost',
         'jquery',
@@ -195,6 +195,195 @@ define(
 
             isAuthenticated: function () {
                 return window.checkoutConfig.payment.ccform.authenticate.active[this.getCode()];
+            },
+
+
+
+
+
+
+
+
+
+            /**
+             * Get list of available credit card types
+             * @returns {Object}
+             */
+            getCcAvailableTypes: function () {
+                return window.checkoutConfig.payment.ccform.availableTypes[this.getCode()];
+            },
+
+            /**
+             * Get payment icons
+             * @param {String} type
+             * @returns {Boolean}
+             */
+            getIcons: function (type) {
+                return window.checkoutConfig.payment.ccform.icons.hasOwnProperty(type) ?
+                    window.checkoutConfig.payment.ccform.icons[type]
+                    : false;
+            },
+
+            /**
+             * Get list of months
+             * @returns {Object}
+             */
+            getCcMonths: function () {
+                return window.checkoutConfig.payment.ccform.months[this.getCode()];
+            },
+
+            /**
+             * Get list of years
+             * @returns {Object}
+             */
+            getCcYears: function () {
+                return window.checkoutConfig.payment.ccform.years[this.getCode()];
+            },
+
+            /**
+             * Check if current payment has verification
+             * @returns {Boolean}
+             */
+            hasVerification: function () {
+                return window.checkoutConfig.payment.ccform.hasVerification[this.getCode()];
+            },
+
+            /**
+             * @deprecated
+             * @returns {Boolean}
+             */
+            hasSsCardType: function () {
+                return window.checkoutConfig.payment.ccform.hasSsCardType[this.getCode()];
+            },
+
+            /**
+             * Get image url for CVV
+             * @returns {String}
+             */
+            getCvvImageUrl: function () {
+                return window.checkoutConfig.payment.ccform.cvvImageUrl[this.getCode()];
+            },
+
+            /**
+             * Get image for CVV
+             * @returns {String}
+             */
+            getCvvImageHtml: function () {
+                return '<img src="' + this.getCvvImageUrl() +
+                    '" alt="' + $t('Card Verification Number Visual Reference') +
+                    '" title="' + $t('Card Verification Number Visual Reference') +
+                    '" />';
+            },
+
+            /**
+             * @deprecated
+             * @returns {Object}
+             */
+            getSsStartYears: function () {
+                return window.checkoutConfig.payment.ccform.ssStartYears[this.getCode()];
+            },
+
+            /**
+             * Get list of available credit card types values
+             * @returns {Object}
+             */
+            getCcAvailableTypesValues: function () {
+                return _.map(this.getCcAvailableTypes(), function (value, key) {
+                    return {
+                        'value': key,
+                        'type': value
+                    };
+                });
+            },
+
+            /**
+             * Get list of available month values
+             * @returns {Object}
+             */
+            getCcMonthsValues: function () {
+                return _.map(this.getCcMonths(), function (value, key) {
+                    return {
+                        'value': key,
+                        'month': value
+                    };
+                });
+            },
+
+            /**
+             * Get list of available year values
+             * @returns {Object}
+             */
+            getCcYearsValues: function () {
+                return _.map(this.getCcYears(), function (value, key) {
+                    return {
+                        'value': key,
+                        'year': value
+                    };
+                });
+            },
+
+            /**
+             * @deprecated
+             * @returns {Object}
+             */
+            getSsStartYearsValues: function () {
+                return _.map(this.getSsStartYears(), function (value, key) {
+                    return {
+                        'value': key,
+                        'year': value
+                    };
+                });
+            },
+
+            /**
+             * Is legend available to display
+             * @returns {Boolean}
+             */
+            isShowLegend: function () {
+                return false;
+            },
+
+            /**
+             * Get available credit card type by code
+             * @param {String} code
+             * @returns {String}
+             */
+            getCcTypeTitleByCode: function (code) {
+                var title = '',
+                    keyValue = 'value',
+                    keyType = 'type';
+
+                _.each(this.getCcAvailableTypesValues(), function (value) {
+                    if (value[keyValue] === code) {
+                        title = value[keyType];
+                    }
+                });
+
+                return title;
+            },
+
+            /**
+             * Prepare credit card number to output
+             * @param {String} number
+             * @returns {String}
+             */
+            formatDisplayCcNumber: function (number) {
+                return 'xxxx-' + number.substr(-4);
+            },
+
+            /**
+             * Get credit card details
+             * @returns {Array}
+             */
+            getInfo: function () {
+                return [
+                    {
+                        'name': 'Credit Card Type', value: this.getCcTypeTitleByCode(this.creditCardType())
+                    },
+                    {
+                        'name': 'Credit Card Number', value: this.formatDisplayCcNumber(this.creditCardNumber())
+                    }
+                ];
             }
 
         });
