548e0a8ba86a76ce6a3cf77dfe300cc8b232315b
Luderson Costa Pereira <luderson@webjump.com.br>
2017-09-20 16:33:52 -0300

    Change method to fetch all card data to use in payment with saved card feature

--- Model/Payment/Transaction/CreditCardToken/Ui/ConfigProvider.php
+++ Model/Payment/Transaction/CreditCardToken/Ui/ConfigProvider.php
@@ -20,28 +20,59 @@ use Webjump\BraspagPagador\Gateway\Transaction\Base\Config\InstallmentsConfigInt
  */
 final class ConfigProvider implements ConfigProviderInterface
 {
+    /**
+     *
+     */
     const CODE = 'braspag_pagador_creditcardtoken';
 
+    /**
+     * @var array
+     */
     protected $installments = [];
 
+    /**
+     * @var
+     */
     protected $installmentsBuilder;
 
+    /**
+     * @var
+     */
     protected $installmentsConfig;
 
+    /**
+     * @var
+     */
     protected $tokensBuilder;
 
+    /**
+     * @var array
+     */
     protected $tokens = [];
 
+    /**
+     * ConfigProvider constructor.
+     *
+     * @param InstallmentsBuilder         $installmentsBuilder
+     * @param TokensBuilder               $tokensBuilder
+     * @param InstallmentsConfigInterface $installmentsConfig
+     */
     public function __construct(
         InstallmentsBuilder $installmentsBuilder,
         TokensBuilder $tokensBuilder,
         InstallmentsConfigInterface $installmentsConfig
-    ) {
+    )
+    {
         $this->setInstallmentsBuilder($installmentsBuilder);
         $this->setTokensBuilder($tokensBuilder);
         $this->setInstallmentsConfig($installmentsConfig);
     }
 
+    /**
+     * Get data to use with saved card payment method
+     *
+     * @return array
+     */
     public function getConfig()
     {
         return [
@@ -52,26 +83,35 @@ final class ConfigProvider implements ConfigProviderInterface
                         'list' => $this->getInstallments(),
                     ],
                     'tokens' => [
-                        'list' => $this->getTokens(),
+                        'list' => $this->getTokens()
                     ],
+                    'cardtokensaved' => $this->getCardTokenSaved()
                 ]
             ]
         ];
     }
 
+    /**
+     * Return installments for credit card
+     *
+     * @return array
+     */
     protected function getInstallments()
     {
         foreach ($this->getInstallmentsBuilder()->build() as $installment) {
             $this->installments[self::CODE][$installment->getId()] = $installment->getLabel();
         }
 
-    	return $this->installments;
+        return $this->installments;
     }
 
+    /**
+     * @return array
+     */
     protected function getTokens()
     {
         $this->tokens[self::CODE] = [];
-        
+
         foreach ($this->getTokensBuilder()->build() as $token) {
             $this->tokens[self::CODE][$token->getToken()] = $token->getAlias();
         }
@@ -79,11 +119,38 @@ final class ConfigProvider implements ConfigProviderInterface
         return $this->tokens;
     }
 
+    /**
+     * @return array
+     */
+    public function getCardTokenSaved()
+    {
+        $cards = [];
+
+        foreach ($this->getTokensBuilder()->build() as $token) {
+            $cards[$token->getMethod()]['brand'] = $token->getBrand();
+            $cards[$token->getMethod()]['card_alias'] = $token->getAlias();
+            $cards[$token->getMethod()]['card_holder_name'] = $token->getData('card_holder_name');
+            $cards[$token->getMethod()]['cpf'] = $token->getTaxvat();
+            $cards[$token->getMethod()]['provider'] = $token->getProvider();
+            $cards[$token->getMethod()]['token'] = $token->getToken();
+        }
+
+        return $cards;
+    }
+
+    /**
+     * @return mixed
+     */
     protected function getInstallmentsBuilder()
     {
         return $this->installmentsBuilder;
     }
 
+    /**
+     * @param InstallmentsBuilder $installmentsBuilder
+     *
+     * @return $this
+     */
     protected function setInstallmentsBuilder(InstallmentsBuilder $installmentsBuilder)
     {
         $this->installmentsBuilder = $installmentsBuilder;
@@ -91,11 +158,19 @@ final class ConfigProvider implements ConfigProviderInterface
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     protected function getTokensBuilder()
     {
         return $this->tokensBuilder;
     }
 
+    /**
+     * @param TokensBuilder $tokensBuilder
+     *
+     * @return $this
+     */
     protected function setTokensBuilder(TokensBuilder $tokensBuilder)
     {
         $this->tokensBuilder = $tokensBuilder;
@@ -103,11 +178,19 @@ final class ConfigProvider implements ConfigProviderInterface
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
     public function getInstallmentsConfig()
     {
         return $this->installmentsConfig;
     }
 
+    /**
+     * @param InstallmentsConfigInterface $installmentsConfig
+     *
+     * @return $this
+     */
     protected function setInstallmentsConfig(InstallmentsConfigInterface $installmentsConfig)
     {
         $this->installmentsConfig = $installmentsConfig;
