78170b989d9a0aeeb7777a04a22a28038ccaa36f
Herbert de Oliveira (Beto) <beto@webjump.com.br>
2018-09-14 13:50:18 -0300

    implementat card token manager

---
+++ Api/CardTokenManagerInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @author      Webjump Core Team <dev@webjump.com.br>
+ * @copyright   2017 Webjump (http://www.webjump.com.br)
+ * @license     http://www.webjump.com.br  Copyright
+ *
+ * @link        http://www.webjump.com.br
+ */
+
+namespace Webjump\BraspagPagador\Api;
+
+interface CardTokenManagerInterface
+{
+    public function registerCardToken($customerId, $paymentMethod, $response);
+}
