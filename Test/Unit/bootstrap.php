db0bd9e4ee6d33be565d3d59822bcf9341956b28
mahsum <mahsum@webjump.com.br>
2018-12-14 11:57:02 -0200

    adding verification to check if bootstrap.php exists

--- Test/Unit/bootstrap.php
+++ Test/Unit/bootstrap.php
@@ -12,14 +12,18 @@ require_once realpath(__DIR__ . '/../../vendor/autoload.php');
 /**
  * @SuppressWarnings(PHPMD.ShortMethodName)
  */
-function __()
-{
-    $argc = func_get_args();
 
-    $text = array_shift($argc);
-    if (!empty($argc) && is_array($argc[0])) {
-        $argc = $argc[0];
+if (function_exists('bootsrap.php')) {
+
+    function __()
+    {
+        $argc = func_get_args();
+
+        $text = array_shift($argc);
+        if (!empty($argc) && is_array($argc[0])) {
+            $argc = $argc[0];
+        }
+
+        return new \Magento\Framework\Phrase($text, $argc);
     }
-
-    return new \Magento\Framework\Phrase($text, $argc);
 }
