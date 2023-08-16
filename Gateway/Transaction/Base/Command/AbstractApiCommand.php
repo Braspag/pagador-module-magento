9355f0c5986a0ec9e517dcf8e00dea64aa01823e
Rodrigo Alexandre <rodrigo.alexandre@engdb.com.br>
2023-08-16 15:29:43 -0300

    3.19.0

--- Gateway/Transaction/Base/Command/AbstractApiCommand.php
+++ Gateway/Transaction/Base/Command/AbstractApiCommand.php
@@ -77,6 +77,7 @@ abstract class AbstractApiCommand implements CommandInterface
 
     protected function prepareRequest($commandSubject, $handle, $typeCard = '')
     {
+
         $request = $this->getRequestBuilder()->build($commandSubject, $typeCard);
 
         if ($this->getRequestValidator()) {
